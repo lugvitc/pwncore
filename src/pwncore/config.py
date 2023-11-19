b94005aaced40c8fe62c7f4588d8cbc9a108e885
KreativeThinker <sagelyeagle@protonmail.com>
2023-11-19 18:09:22 +0530

    Absolute imports, hint and flag endpoints and functions

--- src/pwncore/config.py
+++ src/pwncore/config.py
@@ -1,22 +1,48 @@
-from __future__ import annotations
+from dataclasses import dataclass
 
-import typing as t
+"""
+Sample messages:
+"db_error": "An error occurred, please try again.",
+"port_limit_reached": "Server ran out of ports 💀",
+"ctf_not_found": "CTF does not exist.",
+"container_start": "Container started.",
+"container_stop": "Container stopped.",
+"containers_team_stop": "All team containers stopped.",
+"container_not_found": "You have no running containers for this CTF.",
+"container_already_running": "Your team already has a running container for this CTF.",
+"container_limit_reached": "Your team already has reached the maximum number"
+                           " of containers limit, please stop other unused containers."
+"""
 
-__all__ = (
-    "Config",
-    "BaseConfig",
-)
+msg_codes = {
+    "db_error": 0,
+    "port_limit_reached": 1,
+    "ctf_not_found": 2,
+    "container_start": 3,
+    "container_stop": 4,
+    "containers_team_stop": 5,
+    "container_not_found": 6,
+    "container_already_running": 7,
+    "container_limit_reached": 8,
+    "hint_limit_reached": 9,
+}
 
 
-class Config(t.Protocol):
+@dataclass
+class Config:
     development: bool
+    msg_codes: dict
+    db_url: str
+    docker_url: str | None
+    flag: str
+    max_containers_per_team: int
 
 
-class BaseConfig(Config):
-    __slots__ = ("development",)
-
-    def __init__(self, development: bool) -> None:
-        self.development = development
-
-
-DEV_CONFIG: t.Final[BaseConfig] = BaseConfig(True)
+config = Config(
+    development=True,
+    db_url="sqlite://:memory:",
+    docker_url=None,  # None for default system docker
+    flag="C0D",
+    max_containers_per_team=3,
+    msg_codes=msg_codes,
+)
