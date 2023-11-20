931098b01e92208113e8937f490cd8a852efca1c
KreativeThinker <sagelyeagle@protonmail.com>
2023-11-20 12:24:45 +0530

    Fixed: /team/members/{team_id} and /team/list

--- src/pwncore/routes/team.py
+++ src/pwncore/routes/team.py
@@ -13,9 +13,6 @@ router = APIRouter(prefix="/team", tags=["team"])
 @router.get("/list")
 async def team_list(response: Response):
     teams = await Team.all().values()
-    if not teams:
-        response.status_code = 404
-        return {"msg_code": config.msg_codes["team_not_found"]}
     return teams
 
 
@@ -28,9 +25,8 @@ async def team_login():
 # Unable to test as adding users returns an error
 @router.get("/members/{team_id}")
 async def team_members(team_id: int, response: Response):
-    team = await Team.get_or_none(id=team_id).values_list("members_id", flat=True)
-    if not team:
+    members = await User.filter(team_id=team_id).values()
+    if not members:
         response.status_code = 404
-        return {"msg_code": config.msg_codes["team_not_found"]}
-    members = [await User.get(id=x) for x in team]
+        return {"msg_code": config.msg_codes["user_not_found"]}
     return members
