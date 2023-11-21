86e5c7beff420bb0279d2e61b6a884c747433748
KreativeThinker <sagelyeagle@protonmail.com>
2023-11-21 23:06:53 +0530

    TODO: make hint system pydantic.

--- src/pwncore/routes/ctf/__init__.py
+++ src/pwncore/routes/ctf/__init__.py
@@ -3,7 +3,7 @@ from fastapi import Response
 
 from pwncore.routes.ctf.start import router as start_router
 from pwncore.models.ctf import Problem
-from pwncore.models import Problem, SolvedProblem, Container, Hint, ViewedHint, Team
+from pwncore.models import Problem, SolvedProblem, Container, Hint, ViewedHint
 from pwncore.config import config
 import uuid
 
@@ -25,41 +25,35 @@ def get_team_id():
 
 @router.get("/list")
 async def ctf_list(response: Response):
-    problems = await Problem.all().values()
-    if not problems:
-        response.status_code = 404
-        return {"msg_code": config.msg_codes["ctf_not_found"]}
+    problems = await Problem_Pydantic.from_queryset(Problem.all())
     return problems
 
 
-# @router.get("/flag/{ctf_id}")
-# async def flag_get(ctf_id: int):
-#     flag = await Container.filter(
-#         problem_id=ctf_id, team_id=get_team_id(), flag=flag
-#     ).values()
-#     return flag
-
-
 # For testing purposes only. flag to be passed in body of POST function.
-@router.get("/flag/{ctf_id}/{flag}")
+@router.get("/{ctf_id}/flag/{flag}")
 async def flag_get(ctf_id: int, flag: str, response: Response):
-    problem = await Problem.get_or_none(id=ctf_id)
+    problem = await Problem.exists(id=ctf_id)
     if not problem:
         response.status_code = 404
         return {"msg_code": config.msg_codes["ctf_not_found"]}
 
-    status = await Container.get_or_none(
+    status = await SolvedProblem.get_or_none(team_id=get_team_id(), problem_id=ctf_id)
+    if status:
+        response.status_code = 401
+        return {"msg_code": config.msg_codes["ctf_solved"]}
+
+    check_solved = await Container.get_or_none(
         team_id=get_team_id(), flag=flag, problem_id=ctf_id
     )
-    if status:
-        await SolvedProblem.get_or_create(team_id=get_team_id(), problem_id=ctf_id)
+    if check_solved:
+        await SolvedProblem.create(team_id=get_team_id(), problem_id=ctf_id)
         return {"status": True}
     return {"status": False}
 
 
-@router.get("/hint/{ctf_id}")
+@router.get("/{ctf_id}/hint")
 async def hint_get(ctf_id: int, response: Response):
-    problem = await Problem.get_or_none(id=ctf_id)
+    problem = await Problem.exists(id=ctf_id)
     if not problem:
         response.status_code = 404
         return {"msg_code": config.msg_codes["ctf_not_found"]}
@@ -67,6 +61,7 @@ async def hint_get(ctf_id: int, response: Response):
     viewed = await ViewedHint.filter(team_id=get_team_id()).values_list(
         "hint_id", flat=True
     )
+
     if viewed:
         viewed_hint = await Hint.filter(problem_id=ctf_id, id__in=viewed).values_list(
             "order", flat=True
@@ -88,30 +83,28 @@ async def hint_get(ctf_id: int, response: Response):
     return hint
 
 
-# @router.get("/completed/hint")
-@router.get("/viewed_hints")
-async def completed_hints_get():
-    hints = await ViewedHint.filter(team_id=get_team_id()).values_list(
-        "hint_id", flat=True
+@router.get("/{ctf_id}/viewed_hints")
+async def completed_hints_get(ctf_id: int):
+    viewed_hints = await Hint_Pydantic.from_queryset(
+        Hint.filter(problem_id=ctf_id, hint__team_id=get_team_id()).prefetch_related(
+            "hint"
+        )
     )
-    viewed_hints = [await Hint.get(id=x) for x in hints]
     return viewed_hints
 
 
 @router.get("/completed")
 async def completed_problem_get():
-    problems = await SolvedProblem.filter(team_id=get_team_id()).values_list(
-        "problem_id", flat=True
+    problems = await Problem_Pydantic.from_queryset(
+        Problem.filter(problem__team_id=get_team_id()).prefetch_related("problem")
     )
-    solved_problems = [await Problem.get(id=x) for x in problems]
-    return solved_problems
+    return problems
 
 
 @router.get("/{ctf_id}")
 async def ctf_get(ctf_id: int, response: Response):
-    problem = await Problem.get_or_none(id=ctf_id)
+    problem = await Problem_Pydantic.from_queryset(Problem.filter(id=ctf_id))
     if not problem:
         response.status_code = 404
         return {"msg_code": config.msg_codes["ctf_not_found"]}
-
     return problem
--- src/pwncore/routes/team.py
+++ src/pwncore/routes/team.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from fastapi import APIRouter, Response
-from pwncore.models import Team, User
+from pwncore.models import Team, User, Team_Pydantic, User_Pydantic
 from pwncore.config import config
 
 # Metadata at the top for instant accessibility
@@ -12,7 +12,7 @@ router = APIRouter(prefix="/team", tags=["team"])
 
 @router.get("/list")
 async def team_list(response: Response):
-    teams = await Team.all().values()
+    teams = await Team_Pydantic.from_queryset(Team.all())
     return teams
 
 
@@ -25,6 +25,6 @@ async def team_login():
 # Unable to test as adding users returns an error
 @router.get("/members/{team_id}")
 async def team_members(team_id: int, response: Response):
-    members = await User.filter(team_id=team_id).values()
+    members = await User_Pydantic.from_queryset(User.filter(team_id=team_id))
     # Incase of no members, it just returns an empty list.
     return members
