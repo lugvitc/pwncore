8296e97a1924b2ad530d40b247def63f8e170171
KreativeThinker <sagelyeagle@protonmail.com>
2023-11-24 19:33:08 +0530

    Removed unused import

--- src/pwncore/routes/__init__.py
+++ src/pwncore/routes/__init__.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
 from pwncore.routes import ctf, team
-from pwncore.config import config
 
 # Main router (all routes go under /api)
 router = APIRouter(prefix="/api")
