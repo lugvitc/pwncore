f90e5a32a2ee7c7df7c7061c2c1065387799dea9
KreativeThinker <sagely2005eagle@gmail.com>
2024-01-09 22:19:53 +0530

    chore: /api/leaderboard route

--- src/pwncore/routes/leaderboard.py
+++ src/pwncore/routes/leaderboard.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from fastapi import APIRouter
 from tortoise.functions import Sum
 
-from pwncore.models import SolvedProblem, Team
+from pwncore.models import Team
 
 # Metadata at the top for instant accessibility
 metadata = {"name": "leaderboard", "description": "Operations on the leaderboard"}
@@ -12,10 +12,10 @@ router = APIRouter(prefix="/leaderboard", tags=["leaderboard"])
 
 @router.get("")
 async def fetch_leaderboard():
-    points = (
+    leaderboard = dict(
         await Team.all()
         .annotate(team_points=Sum("solved_problem__problem__points"))
         .values_list("name", "team_points")
     )
     # points = sorted(points, key=lambda x: x[0], reverse=True)
-    return points
+    return leaderboard
