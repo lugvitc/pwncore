0b729e4c9d75cb93d6e6521057daf6f1b2b5f8f5
mradigen <55953083+mradigen@users.noreply.github.com>
2024-01-14 02:52:24 +0530

    feat: Add ports to /api/team/containers

--- src/pwncore/routes/team.py
+++ src/pwncore/routes/team.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from tortoise.transactions import atomic
 
 from pwncore.config import config
-from pwncore.models import Team, User, Team_Pydantic, User_Pydantic, Container
+from pwncore.models import Team, User, Team_Pydantic, User_Pydantic, Container, Ports
 from pwncore.routes.auth import RequireJwt
 
 # Metadata at the top for instant accessibility
@@ -85,7 +85,13 @@ async def remove_member(user_info: UserRemoveBody, response: Response, jwt: Requ
 
 @router.get("/containers")
 async def get_team_containers(response: Response, jwt: RequireJwt):
-    containers = await Container.filter(
-        team_id=jwt["team_id"]
-    ).values("problem_id")
-    return containers
+    containers = await Container.filter(team_id=jwt["team_id"]).prefetch_related("ports")
+    result = []
+
+    for container in containers:
+        result.append({
+            "id": container.problem_id,
+            "ports": await container.ports.all().values_list("port", flat=True),
+        })
+
+    return result
