e63ce2067f4d83521c5ddd71291373130aa5a1c8
celestix <i@anodite.me>
2025-02-20 01:21:01 +0530

    refactor: simplify port exposing

--- src/pwncore/models/ctf.py
+++ src/pwncore/models/ctf.py
@@ -31,9 +31,11 @@ class BaseProblem(Model):
 
 class Problem(BaseProblem):
     image_name = fields.TextField()
-    image_config: fields.Field[dict[str, Any]] = fields.JSONField(
-        null=True
-    )  # type: ignore[assignment]
+    
+    # commenting it for now, may be used later
+    # image_config: fields.Field[dict[str, Any]] = fields.JSONField(
+    #     null=True
+    # )  # type: ignore[assignment]
 
     mi = fields.IntField(default=50)
     ma = fields.IntField(default=500)
@@ -43,7 +45,7 @@ class Problem(BaseProblem):
     hints: fields.ReverseRelation[Hint]
 
     class PydanticMeta:
-        exclude = ["image_name", "image_config", "mi", "ma", "visible"]
+        exclude = ["image_name", "mi", "ma", "visible"]
 
     async def _solves(self) -> int:
         return await SolvedProblem.filter(problem=self).count()
